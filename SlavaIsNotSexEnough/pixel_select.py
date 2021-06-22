3c51efe229bb5ba4e1a9f256b74ed3d382faee5a
iNe1t <fantiktop212@gmail.com>
2021-06-22 14:08:14 +0300

    commit

--- SlavaIsNotSexEnough/pixel_select.py
+++ SlavaIsNotSexEnough/pixel_select.py
@@ -1,9 +1,41 @@
 from PIL import Image
-an_image = Image.open("Sample.jpg")
+import cv2
+import os
 
-sequence_of_pixels = an_image.getdata()
-list_of_pixels = list(sequence_of_pixels)
+img1 = input("Введи имя картинки для создания тепловой карты: ")
+img2 = input("Введи имя тепловой картинки: ")
 
-print(list_of_pixels)
+if img1 != "":
+    # Считываем картинку
+    img = cv2.imread(img1)
+    # Пропускаем через тепловой фильтр
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    heatmap_img = cv2.applyColorMap(gray_img, cv2.COLORMAP_JET)
+    # Заменяем исходник на обработанную картинку
+    os.system("del "+ str(img1))
+    cv2.imwrite(img1, heatmap_img)
+
+# Получаем все пиксели теплого спектра
+warm_image = Image.open("warm_colors.jpg")
+warm_pixels = warm_image.getdata()
+list_of_warm_pixels = list(warm_pixels)
+# Получаем картинку для определения заполнения
+test_image = Image.open(img2)
+test_pixels = test_image.getdata()
+list_of_test_pixels = list(test_pixels)
+
+# Считаем...
+percent_of_warm_pixels = 0
+for pixel in list_of_test_pixels[::100]:
+    if pixel in list_of_warm_pixels:
+        percent_of_warm_pixels = percent_of_warm_pixels + 1
+
+aaa = len(list_of_test_pixels)/100
+print(aaa)
+percent = percent_of_warm_pixels / aaa *1000
+print(percent)
+           
+
+
 
 
