72ed9691fa4e408848778e4ce0d006d6cd8019cc
iNe1t <fantiktop212@gmail.com>
2021-06-22 16:02:41 +0300

    commit

--- SlavaIsNotSexEnough/pixel_select.py
+++ SlavaIsNotSexEnough/pixel_select.py
@@ -2,18 +2,18 @@ from PIL import Image
 import cv2
 import os
 
-img1 = input("Введи имя картинки для создания тепловой карты: ")
+img_for_map = input("Введи имя картинки для создания тепловой карты: ")
 img2 = input("Введи имя тепловой картинки: ")
 
-if img1 != "":
+if img_for_map != "":
     # Считываем картинку
-    img = cv2.imread(img1)
+    img = cv2.imread(img_for_map)
     # Пропускаем через тепловой фильтр
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     heatmap_img = cv2.applyColorMap(gray_img, cv2.COLORMAP_JET)
     # Заменяем исходник на обработанную картинку
-    os.system("del "+ str(img1))
-    cv2.imwrite(img1, heatmap_img)
+    os.system("del "+ str(img_for_map))
+    cv2.imwrite(img_for_map, heatmap_img)
 
 # Получаем все пиксели теплого спектра
 warm_image = Image.open("warm_colors.jpg")
@@ -26,16 +26,15 @@ list_of_test_pixels = list(test_pixels)
 
 # Считаем...
 percent_of_warm_pixels = 0
-for pixel in list_of_test_pixels[::100]:
+for pixel in list_of_test_pixels: # [::100]
     if pixel in list_of_warm_pixels:
         percent_of_warm_pixels = percent_of_warm_pixels + 1
 
-aaa = len(list_of_test_pixels)/100
-print(aaa)
-percent = percent_of_warm_pixels / aaa *1000
+print("Теплых пикселей: "+ str(percent_of_warm_pixels))
+all_pic_pixels = len(list_of_test_pixels)
+print("Всего пикселей: "+ str(all_pic_pixels))
+divide = percent_of_warm_pixels / all_pic_pixels
+print("Поделили: " + str(divide))
+percent = divide *100
 print(percent)
            
-
-
-
-
