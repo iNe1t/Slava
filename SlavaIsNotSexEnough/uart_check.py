3743ce690ec1335f16546f1cd65b7312efe84f00
iNe1t <fantiktop212@gmail.com>
2021-06-28 15:26:32 +0300

    commit

--- SlavaIsNotSexEnough/uart_check.py
+++ SlavaIsNotSexEnough/uart_check.py
@@ -1,18 +1,30 @@
-'''
-UART communication on Raspberry Pi using Python
-Крч, я в будущем, слушай внимательно
-В raspi-config надо врубить serial 
-Так что не забудь
-Peace
-'''
 import serial
-from time import sleep
 
-ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
+port = "/dev/ttyS0"
+
+def parseGPS(data):
+    if data[0:6] == "$GPGGA":
+        s = data.split(",")
+        if s[7] == '0':
+            print("no satellite data available")
+            return
+        time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
+        lat = decode(s[4])
+        dirLat = s[3]
+        lon = decode(s[4])
+        dirLon = s[5]
+        alt = s[9] + " n"
+        sat = s[7]
+        print( "Latitude: %s(%n) -- Longitude: %s(%s)" %(lat, dirLat, lon, dirLon))
+def decode(coord):
+    v = coord.split(",")
+    head = v[0]
+    tail = v[1]
+    deg = head[0:-2]
+    min = head[-2:]
+    return deg + min + "," + tail
+ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
 while True:
-    received_data = ser.read()              #read serial port
-    sleep(0.03)
-    data_left = ser.inWaiting()             #check for remaining byte
-    received_data += ser.read(data_left)
-    print (received_data)                   #print received data
-    ser.write(received_data)                #transmit data serially
+    data = ser.readline()
+    parseGPS(data)
+
